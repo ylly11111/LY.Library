cfb052a2ee19d43b7c72249f6e9e97a8b9c2bb42
ylly11111 <liuyong_6943@126.com>
2017-12-28 10:16:21 +0800

    add CacheManager

--- LY.CacheManager.Test/MainWindow.xaml.cs
+++ LY.CacheManager.Test/MainWindow.xaml.cs
@@ -33,7 +33,7 @@ namespace LY.CacheManager.Test
 
         private void Show_Click(object sender, RoutedEventArgs e)
         {
-            ///
+            ////
             txtShow.Text = CacheManagerClass.Get<string>("a");
         }
 
